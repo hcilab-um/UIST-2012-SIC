f821035a27c71898c5a6e74b7c7e110157e005d9
Jango Guo <jango.boogaloo@gmail.com>
2012-09-06 12:38:54 -0500

    PhidgetSpike

---
+++ PhidgetSpike/PhidgetSpike/PhidgetSpike.cpp
@@ -0,0 +1,13 @@
+// PhidgetSpike.cpp : main project file.
+
+#include "stdafx.h"
+#include <phidget21.h>
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+	Console::ReadLine();
+    return 0;
+}
